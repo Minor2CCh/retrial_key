474901060927308283d8fc244a40079220564f41
Minor2CCh <minormugen716@gmail.com>
2026-02-28 10:47:20 +0900

    Changed the Key Way Mold class from unique to normal item.

--- common/src/main/java/com/Minor2CCh/retrial_key/registry/ModItemGroup.java
+++ common/src/main/java/com/Minor2CCh/retrial_key/registry/ModItemGroup.java
@@ -9,14 +9,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 
-public class ModItemGroup {
+public final class ModItemGroup {
+    private ModItemGroup() {}
     public static final DeferredRegister<ItemGroup> TABS =
             DeferredRegister.create(Retrial_key.MOD_ID, RegistryKeys.ITEM_GROUP);
 
     public static final RegistrySupplier<ItemGroup> TAB_RETRIAL_KEY = TABS.register("retrial_key_group",
             () -> CreativeTabRegistry.create(
                     Text.translatable("itemGroup.retrial_key"),
-                    () ->new ItemStack(ModItems.RETRIAL_KEY.get())
+                    () -> new ItemStack(ModItems.RETRIAL_KEY.get())
             )
     );
     public static void init(){
